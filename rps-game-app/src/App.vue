bb9b4f898e7338e3f3ec37ce1c29d7841d6694f9
cc211018 <cc211018@fhstp.ac.at>
2023-01-31 20:33:15 +0100

    set-up logic and started with template

--- rps-game-app/src/App.vue
+++ rps-game-app/src/App.vue
@@ -1,7 +1,94 @@
 <script setup>
+import {ref, computed, onMounted} from 'vue'
+
+const wins = ref(0)
+const losses = ref(0)
+const draws = ref(0)
+
+const playerChoice = ref(null)
+const computerChoice = ref(null)
+const verdict = ref(null)
+
+const outcomes = {
+  rock: {
+    rock: 'draw',
+    paper: 'loss',
+    scissors: 'win'
+  },
+  paper: {
+    rock: 'win',
+    paper: 'draw',
+    scissors: 'loss'
+  },
+  scissors: {
+    rock: 'loss',
+    paper: 'win',
+    scissors: 'draw'
+  }
+}
+
+const winPercentage = computed(() => {
+  const total = wins.value + losses.value + draws.value
+  return total ? (wins.value / total)*100 : 0
+})
+
+const play = c => {
+  playerChoice.value = c
+
+  const choices = ['rock', 'paper', 'scissors']
+  const random = Math.floor(Math.random() * choices.length)
+  computerChoice.value = choices[random]
+
+  const outcome = outcomes[c][computerChoice.value]
+
+  if (outcome === 'win') {
+    wins.value++
+    verdict.value = "You win!"
+  } else if (outcome === 'loss') {
+    losses.value++
+    verdict.value = "You lose!"
+  } else {
+    draws.value++
+    verdict.value = "It is a draw!"
+  }
+
+  SaveGame()
+}
+
+const SaveGame = () => {
+  localStorage.setItem('wins', wins.value)
+  localStorage.setItem('losses', losses.value)
+  localStorage.setItem('draws', draws.value)
+}
+
+const LoadGame = () => {
+  wins.value = localStorage.getItem('wins')
+  losses.value = localStorage.getItem('losses')
+  draws.value = localStorage.getItem('draws')
+}
+
+const ResetGame = () => {
+  playerChoice.value = null
+  computerChoice.value = null
+  verdict.value = null
+}
+
+onMounted(() => {
+  LoadGame()
+  window.addEventListener('keypress', e => {
+    if (e.key === 'r') {
+      ResetGame()
+    }
+  })
+})
+
 
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <div class="bg-amber-700 text-white text-center min-h-screen flex flex-col">
+    <header class="container mx-auto p-6">
+      <h1 class="text-3xl font-bold">My really cool Rock, Paper, Scissors Game</h1>
+    </header>
+  </div>
 </template>
--- rps-game-app/tailwind.config.cjs
+++ rps-game-app/tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./index.html",
-      ".src/**/*.{vue, js, ts, jsx, tsx}"
+    './src/**/*.html',
+    './src/**/*.vue',
   ],
   theme: {
     extend: {},
