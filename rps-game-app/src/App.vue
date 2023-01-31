48255b0b582741072fc6b9472851bdf99616d313
cc211018 <cc211018@fhstp.ac.at>
2023-01-31 21:22:54 +0100

    finished template and fixed errors in logic

--- rps-game-app/src/App.vue
+++ rps-game-app/src/App.vue
@@ -29,7 +29,7 @@ const outcomes = {
 
 const winPercentage = computed(() => {
   const total = wins.value + losses.value + draws.value
-  return total ? (wins.value / total)*100 : 0
+  return total ? (wins.value / total) * 100 : 0
 })
 
 const play = c => {
@@ -62,9 +62,9 @@ const SaveGame = () => {
 }
 
 const LoadGame = () => {
-  wins.value = localStorage.getItem('wins')
-  losses.value = localStorage.getItem('losses')
-  draws.value = localStorage.getItem('draws')
+  wins.value = parseInt(localStorage.getItem('wins')) || 0
+  losses.value = parseInt(localStorage.getItem('losses')) || 0
+  draws.value = parseInt(localStorage.getItem('draws')) || 0
 }
 
 const ResetGame = () => {
@@ -90,5 +90,45 @@ onMounted(() => {
     <header class="container mx-auto p-6">
       <h1 class="text-3xl font-bold">My really cool Rock, Paper, Scissors Game</h1>
     </header>
+
+    <main class="container mx-auto p-6 flex-1">
+      <div v-if="playerChoice === null" class="flex items-center justify-center -mx-6">
+        <button @click="play('rock')"
+                class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-pink-500">
+          <img src="./assets/rock.svg" alt="Rock" class="w-full"/>
+        </button>
+
+        <button @click="play('paper')"
+                class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-cyan-500">
+          <img src="./assets/paper.svg" alt="Paper" class="w-full"/>
+        </button>
+
+        <button @click="play('scissors')"
+                class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-yellow-500">
+          <img src="./assets/scissor.svg" alt="Scissors" class="w-full"/>
+        </button>
+      </div>
+
+      <div v-else class="text-2xl mb-4">
+        You picked <span class="font-bold">{{ playerChoice }}</span> and the computer picked <span
+          class="font-bold">{{ computerChoice }}</span>.
+
+
+        <div class="text-5xl mb-12">
+          {{ verdict }}
+        </div>
+
+        <button @click="ResetGame" class="bg-pink-500 text-lg py-2 px-4">Reset Game</button>
+      </div>
+
+      <div class="mt-12 text-3xl mb-4">
+        {{ wins }} : {{ draws }} : {{ losses }}
+      </div>
+
+      <div class="text-lg">
+        Win rate: {{ Math.round(winPercentage) }}%
+      </div>
+
+    </main>
   </div>
 </template>
